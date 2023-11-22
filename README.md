2186878226ba5918a479f568f180c607e8db16d7
Emiliano Perez <emi.perez997@gmail.com>
2023-11-22 16:42:24 -0300

    [ADD] Signin Component

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 
 ## Issues a resolver para completar la prueba
 
-- [ ] 1. Implementar un sistema de login y registro de usuarios.
-- [ ] 2. Los usuarios deben estar guardados en el localStorage.
-- [ ] 3. Navbar dinámico. Que si el usuario no está logueado no pueda ver la tienda
-- [ ] 4. Restringir las rutas si el usuario no está logueado.
+- [ ] 1. Maquetación de las vistas de usuarios
+- [ ] 2. Implementar un sistema de login y registro de usuarios.
+- [ ] 3. Los usuarios deben estar guardados en el localStorage.
+- [ ] 4. Navbar dinámico. Que si el usuario no está logueado no pueda ver la tienda
+- [ ] 5. Restringir las rutas si el usuario no está logueado.
--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -1,5 +1,62 @@
 function SingIn() {
-  return <p>Sign in</p>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { email, password } = e.target.elements;
+
+    if (!email.value || !password.value) {
+      alert("Please fill all fields");
+    }
+  };
+
+  return (
+    <>
+      <div className="flex relative items-center justify-center w-80 mb-4 text-center">
+        <h1 className="font-medium text-xl">Products</h1>
+      </div>
+
+      <div>
+        <form
+          action=""
+          className="flex flex-col gap-3 w-80 justify-center items-center"
+          onSubmit={handleSubmit}
+        >
+          <div className="flex justify-between w-80">
+            <label htmlFor="email" className="font-semibold">
+              Email:
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              className="border border-transparent border-b-black focus:outline-none"
+            />
+          </div>
+          <div className="flex justify-between w-80">
+            <label htmlFor="password" className="font-semibold">
+              Password:{" "}
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="border border-transparent border-b-black focus:outline-none"
+            />
+          </div>
+
+          <div className="flex flex-col mt-11 gap-5">
+            <button className="border border-black w-80 p-2 rounded-lg bg-black text-white focus:outline-none ">
+              Login
+            </button>
+
+            <button className="border border-black w-80 p-2 rounded-lg focus:outline-none ">
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export { SingIn };
